f9dc9c9dc961b20fbbad77de7c08bad447cba152
eun <springono@naver.com>
2023-02-21 14:55:17 +0900

    message1

--- FirstJava/src/TeamCode/Hello.java
+++ FirstJava/src/TeamCode/Hello.java
@@ -3,8 +3,8 @@ package TeamCode;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("¾È³ç ¼¼»ó¾Æ!");
-		System.out.println("Hello~");
+		System.out.println("Hello!");
+		System.out.println("World~");
 
 	}
 
